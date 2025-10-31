fe993ea6dd9c28fe5ce2d7a70f7c0a583bd16864
FelipeBarretoB <FelipeBarretoB@users.noreply.github.com>
2025-10-31 03:11:50 +0000

    deploy: ab07bfa01b4c7a17e7eb9dff1b440f0f38bfeef3

--- content/js/dashboard.js
+++ content/js/dashboard.js
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 100, 100, 100.0, 136074.93, 135592, 136470, 136077.0, 136459.0, 136466.65, 136469.99, 0.7327188264775275, 2.021416684374038, 0.0], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Petición HTTP", 100, 100, 100.0, 136074.93, 135592, 136470, 136077.0, 136459.0, 136466.65, 136469.99, 0.7327188264775275, 2.021416684374038, 0.0], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 100, 100, 100.0, 132953.79000000007, 132400, 136484, 132894.5, 133262.9, 133264.0, 136483.91, 0.7280510800637773, 2.0085393566212604, 0.0], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Petición HTTP", 100, 100, 100.0, 132953.79000000007, 132400, 136484, 132894.5, 133262.9, 133264.0, 136483.91, 0.7280510800637773, 2.0085393566212604, 0.0], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
